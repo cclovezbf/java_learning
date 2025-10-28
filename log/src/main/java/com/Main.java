4f075f050a3b787a59c5e20dfaafd05149411d70
v_chicchen <v_chicchen@tencent.com>
2025-10-28 20:14:55 +0800

    1

--- log/src/main/java/com/Main.java
+++ log/src/main/java/com/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
     public static void main(String[] args) {
             logger.info("Hello World 0");
+            logger.info("Hello World 1");
     }
 }
