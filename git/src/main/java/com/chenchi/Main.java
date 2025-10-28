c9a89d7eeb44426c2b8b4e04b8fe28ba92e91ab0
cclovezbf <50588928+cclovezbf@users.noreply.github.com>
2025-10-28 20:26:20 +0800

    Update Main.java
    2222

--- git/src/main/java/com/chenchi/Main.java
+++ git/src/main/java/com/chenchi/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! add ");
         System.out.println("Hello world!");
-        System.out.println("Hello world 1111111");
+        System.out.println("Hello world 2222222222");
     }
 }
