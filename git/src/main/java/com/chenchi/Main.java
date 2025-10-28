8c5fdb6107d343c95d370ac52bb42e0f11c46943
v_chicchen <v_chicchen@tencent.com>
2025-10-28 19:59:02 +0800

    111111

--- git/src/main/java/com/chenchi/Main.java
+++ git/src/main/java/com/chenchi/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world! add ");
         System.out.println("Hello world!");
         System.out.println("Hello world! 1111111");
+        System.out.println("Hello world! 22222222");
     }
 }
