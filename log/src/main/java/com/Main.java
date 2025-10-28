a8bdadc57ee4849f1cc5cefdfa73e9768fa517dd
v_chicchen <v_chicchen@tencent.com>
2025-10-28 20:17:42 +0800

    0

--- log/src/main/java/com/Main.java
+++ log/src/main/java/com/Main.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 public class Main {
     public static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
     public static void main(String[] args) {
-            logger.info("Hello World 0");
+            logger.info("Hello World 00");
+            logger.info("Hello World 1");
+            logger.info("Hello World 2");
     }
 }
